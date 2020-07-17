329ec09e54d6b75c6005c7211907ec8d157dd0ef
ayandu <nbc.555.an@gmail.com>
2020-07-17 16:05:26 +0200

    init commit

---
+++ src/main/java/io/ayandu/kwsu/KwsuApplication.java
@@ -0,0 +1,13 @@
+package io.ayandu.kwsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwsuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ayandu/kwsu/KwsuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ayandu.kwsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
